c95d0cca91e30bfed3d31e73bbb52689d5963670
Zlorblak <correoelectronico2036@gmail.com>
2024-11-01 03:47:44 +0000

    Add files via upload

--- JS/function.js
+++ JS/function.js
@@ -1,30 +1,21 @@
-const header = document.querySelector('header');
-const altura = header.clientHeight;
-
 const Search_Options = document.querySelector('.Search-Option');
 
-document.getElementById('Lupa').addEventListener('click',function(){
-    if(altura === 52) {
-        Search_Options.style.top='-8px';
-    } else {
-        Search_Options.style.top='10px';
-    }
+
+document.getElementById('Lupa').addEventListener('click',function(){    
+    Search_Options.style.top='28px';    
 });
 
 const Menu = document.querySelector('.Menu-Options');
 
-document.getElementById('Close').addEventListener('click',function(){
-    Search_Options.style.top='-100%';
-    Menu.style.right='-100%'
-});
-
 document.getElementById('Menu').addEventListener('click',function(){
     Menu.style.right='0';
+    Menu.style.top='28px';
+});
 
-    if(altura === 70) {
-        Menu.style.top='10px';  
-    } else {
-        Menu.style.top='28px';
-    }
-    
+document.getElementById('Close').addEventListener('click',function(){
+    Menu.style.right='-100%';
+})
+
+document.getElementById('Close-1').addEventListener('click',function(){
+    Search_Options.style.top = '-100%';
 });
